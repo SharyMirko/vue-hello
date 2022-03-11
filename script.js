8561b2231a5312503973a76309aadcdf1d84058b
Mirko Moschella <shary.mirko@gmail.com>
2022-03-11 16:20:23 +0100

    esercizio quasi fatto

--- script.js
+++ script.js
@@ -2,6 +2,11 @@ const app = new Vue({
     el: '#root',
     data: {
       hello: 'Hello with Vue.js!',
-      vueImg: 'https://picsum.photos/200/200'
+      vueImg: 'https://picsum.photos/200/200',
+    },
+    methods: {
+      hi: function() {
+        return this.hello = 'ciao'
+      }
     }
   })
